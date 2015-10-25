5e41562a00e5d998de1ba567d9b032cea09234ba
ebranddit <ebranddit@gmail.com>
2015-10-24 23:19:18 -0700

    sass it up

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,26 +2,22 @@
 
 angular.module('jbuilderApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    var navItems = [];
+    var menuItems = [];
 
-    navItems.push({label: "Tour", url: "/tour"});
-    navItems.push({label: "Create", url: "/create"});
-    navItems.push({label: "Manage", url: "/manage"});
+    menuItems.push({title: "Home", link: "/"});
+    menuItems.push({title: "Features", link: "/feature"});
+    menuItems.push({title: "Plans", link: "/plan"});
+    menuItems.push({title: "Contact", link: "/contact"});
 
-    $scope.navItems = navItems;
 
-    $scope.xfShow = false;
+    $scope.menuItems = menuItems;
 
-    $scope.toggleXF = function(){
-      $scope.xfShow = !$scope.xfShow;
+    $scope.menuCollapsed = true;
+
+    $scope.toggleMenu = function(){
+      $scope.menuCollapsed = !$scope.menuCollapsed;
     }
-    
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
 
-    $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
