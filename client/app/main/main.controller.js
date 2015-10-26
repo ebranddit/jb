d90e2e35fac031cf4772fb780e2330aa3f6c0102
ebranddit <ebranddit@gmail.com>
2015-10-25 21:41:57 -0700

    sassify

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,5 +2,10 @@
 
 angular.module('jbuilderApp')
   .controller('MainCtrl', function ($scope, $http) {
+    var tiles = [];
 
+    tiles.push({title: "Create", link: "/create", icon: "building-o"});
+    tiles.push({title: "Manage", link: "/login", icon: "gear"});
+
+    $scope.tiles = tiles;
   });
